b354ff0c3fce637734366b945b5fe71c41702e3d
oat431 <oat431@gmail.com>
2023-06-14 03:03:57 +0700

    🛠 setup: websocket project

---
+++ src/main/java/panomete/spb3ws/Spb3wsApplication.java
@@ -0,0 +1,13 @@
+package panomete.spb3ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spb3wsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spb3wsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/panomete/spb3ws/Spb3wsApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.spb3ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spb3wsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
